894f4ef8de0353d8cf99435348e9d54d286d3460
rahi-7 <rabera131@gmail.com>
2024-07-12 12:01:09 -0700

    initial commit

--- firebase.js
+++ firebase.js
@@ -1,15 +1,22 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection } from "firebase/firestore"
+import { getFirestore, collection } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD_uXh8KfNe11S8qhfQuB_RAzIHrKFTOHE",
-  authDomain: "react-notes-7a013.firebaseapp.com",
-  projectId: "react-notes-7a013",
-  storageBucket: "react-notes-7a013.appspot.com",
-  messagingSenderId: "370854322953",
-  appId: "1:370854322953:web:3e20fb09a3b7f61fc329d8"
+  apiKey: "AIzaSyA1vXyPYKrDFFIWrkiMKvuNy19m4PCrjTQ",
+  authDomain: "react-note-f9446.firebaseapp.com",
+  projectId: "react-note-f9446",
+  storageBucket: "react-note-f9446.appspot.com",
+  messagingSenderId: "315639826573",
+  appId: "1:315639826573:web:c7b1e8dbf27eecc214bac4",
+  measurementId: "G-Y8MFEHEY8B"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
 export const notesCollection = collection(db, "notes")
